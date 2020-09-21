6675e6f3f50561bc814651f37d5a773a118463bd
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-21 13:05:42 +0530

    Added the Tile component and the font awesome icon list to be used

---
+++ src/components/Tile.js
@@ -0,0 +1,19 @@
+// Imports
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// Tile component for individual tiles
+function Tile(props){
+    // icon is the design and onClick is the handler
+    const { index, icon, onClick } = props
+
+    // Just a plain button with a onClick event
+    return (
+        <button data-testid={index} onClick={onClick}>
+            <FontAwesomeIcon icon={icon} size="3x"/>
+        </button>
+    )
+}
+
+// Exporting the created component
+export default Tile
