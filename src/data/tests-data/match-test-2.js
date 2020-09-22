7c34ad6b53d3b8dca5a0944102e3c4e3e183207a
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-22 17:06:04 +0530

    Added tests for the helper functions

---
+++ src/data/tests-data/match-test-2.js
@@ -0,0 +1,32 @@
+const tileList2 = [
+    {
+        icon: "bomb",
+        enabled: true,
+        found: false,
+        finder: "",
+        matchId: 0
+    },
+    {
+        icon: "bomb",
+        enabled: false,
+        found: false,
+        finder: "",
+        matchId: 0
+    },
+    {
+        icon: "radiation",
+        enabled: true,
+        found: false,
+        finder: "",
+        matchId: 1
+    },
+    {
+        icon: "radiation",
+        enabled: false,
+        found: false,
+        finder: "",
+        matchId: 1
+    }
+]
+
+export default tileList2
---
+++ src/data/tests-data/match-test-3.js
@@ -0,0 +1,32 @@
+const tileList3 = [
+    {
+        icon: "brain",
+        enabled: true,
+        found: false,
+        finder: "",
+        matchId: 0
+    },
+    {
+        icon: "dragon",
+        enabled: false,
+        found: false,
+        finder: "",
+        matchId: 1
+    },
+    {
+        icon: "brain",
+        enabled: true,
+        found: false,
+        finder: "",
+        matchId: 0
+    },
+    {
+        icon: "dragon",
+        enabled: false,
+        found: false,
+        finder: "",
+        matchId: 1
+    }
+]
+
+export default tileList3
