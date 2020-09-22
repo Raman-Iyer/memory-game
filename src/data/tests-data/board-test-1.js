00685728560f3371029f0ae10627dcfb83bb6a3f
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-22 16:42:51 +0530

    Added tests for Board Component

---
+++ src/data/tests-data/board-test-1.js
@@ -0,0 +1,32 @@
+const tileList1 = [
+    {
+        icon: "bomb",
+        enabled: true,
+        found: true,
+        finder: "p1",
+        matchId: 0
+    },
+    {
+        icon: "bomb",
+        enabled: true,
+        found: true,
+        finder: "p1",
+        matchId: 0
+    },
+    {
+        icon: "radiation",
+        enabled: true,
+        found: true,
+        finder: "p2",
+        matchId: 1
+    },
+    {
+        icon: "radiation",
+        enabled: true,
+        found: true,
+        finder: "p2",
+        matchId: 1
+    }
+]
+
+export default tileList1
