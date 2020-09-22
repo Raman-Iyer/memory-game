61d224244f06553eab78957c6ce6fb4d67f8c5c6
RamanIyer <raman.iyer@deuexsolutions.com>
2020-09-22 18:31:44 +0530

    Updates

---
+++ precache-manifest.eccdc95c26b8cd85054cbbafa5030580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe0361a1334775485e195ab39b4c745",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "27326319349af7064112",
+    "url": "/memory-game/static/css/main.6974a3d0.chunk.css"
+  },
+  {
+    "revision": "007fa65c50b9417e838b",
+    "url": "/memory-game/static/js/2.e5c5d244.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.e5c5d244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27326319349af7064112",
+    "url": "/memory-game/static/js/main.452f6a6d.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
