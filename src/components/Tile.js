88a6f776436909eb5e7fbf2d754ab1ab6b894d95
RamanIyer <raman.iyer@deuexsolutions.com>
2020-12-07 18:06:18 +0530

    full commit

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -18,7 +18,7 @@ function Tile(props){
         <button className={classes} data-testid={index} onClick={onClick} style={{flexGrow: 0, flexShrink: 0, flexBasis: `${tileWidth}%`}}>
             <FontAwesomeIcon data-testid={icon} icon={icon} fixedWidth/>
         </button>
-        <div>this is a tile</div>
+        <div>this is a epic tile</div>
         </>
     )
 }
